33e14d6354d58d66ce85c58420d2768da31001af
Triton Dev <goldensoftdev@gmail.com>
2017-05-12 08:28:21 -0700

    add timeline owner helper

--- app/helpers/timelines_helper.rb
+++ app/helpers/timelines_helper.rb
@@ -1,2 +1,6 @@
 module TimelinesHelper
+  def is_owner?(user, timeline)
+    shared_timeline = SharedTimeline.find_by(timeline_id: timeline.id)
+    return user.id == shared_timeline.user_id
+  end
 end
