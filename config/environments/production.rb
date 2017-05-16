04db28617f8d1ce029f93c074bfe33e3fedbc3e8
Triton Dev <erwinbakkerwin@gmail.com>
2017-05-16 08:55:00 -0700

    change auth method for smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
-    authentication: :login,
+    authentication: "plain",
     enable_starttls_auto: true,
     user_name: "goldensoftdev@gmail.com",
     password: "poseidon88"
