d0285a43d894344ff54824fa44f1174125d2611c
Triton Dev <erwinbakkerwin@gmail.com>
2017-05-16 08:59:01 -0700

    change domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "gmail.com",
+    domain: "dev-timeline.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "goldensoftdev@gmail.com",
