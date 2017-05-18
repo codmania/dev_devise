8383950f983bf1bf775f4e083b254bbd8fdb1389
Triton Dev <erwinbakkerwin@gmail.com>
2017-05-18 07:10:54 -0700

    add validate for timeline status

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -1,7 +1,9 @@
 class Timeline < ActiveRecord::Base
   has_many :events, dependent: :destroy
   has_many :shared_timelines, dependent: :destroy
-  validates :name, presence: true, length: { minimum: 3 }
   belongs_to :user
   belongs_to :timeline_status
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :timeline_status_id, presence: true
 end
