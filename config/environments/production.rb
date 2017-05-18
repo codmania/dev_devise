184c704623e1d92da3778d2207e68a6d10b2ab29
Triton Dev <erwinbakkerwin@gmail.com>
2017-05-18 09:31:38 -0700

    change host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
     password: "poseidon88"
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'https://dev-timeline.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'dev-timeline.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
